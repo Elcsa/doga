f6939e19e0a48f3a772945103fe18b667569120f
Elcsa <81393593+Elcsa@users.noreply.github.com>
2022-09-29 15:42:34 +0200

    oran eddig jutotam

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
+import 'bootstrap/dist/js/bootstrap.bundle.js'
+import 'bootstrap/dist/css/bootstrap.css'
 import './style.css';
-
+console.log("javascript betoltve")
+let gomb = document.getElementById("sotet").addEventListener("click", function(sotet) { sotet })
